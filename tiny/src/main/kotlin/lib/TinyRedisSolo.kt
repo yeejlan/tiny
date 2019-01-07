22e04ff3d3eeab6c7ec6cedf9f6f1541d4cee219
louis yu <yf_hello@163.com>
2019-01-07 18:44:49 +0800

    add TinyRedisSolo

---
+++ tiny/src/main/kotlin/lib/TinyRedisSolo.kt
@@ -0,0 +1,50 @@
+package tiny.lib
+
+import io.lettuce.core.RedisClient
+import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.RedisURI
+
+import java.time.Duration
+
+private val connectTimeout = Duration.ofSeconds(3)
+
+class TinyRedisSolo(host: String, port:Int = 6379, database: Int = 1, timeout: Duration = connectTimeout) {
+	private val _host: String
+	private val _port: Int
+	private val _database: Int
+	private val _timeout: Duration
+	private lateinit var _client: RedisClient
+	private var _conn: StatefulRedisConnection<String, String>? = null
+
+	init {
+		_host = host
+		_port = port
+		_database = database
+		_timeout = timeout
+		settle()
+	}
+
+	fun settle() {
+		val uri = RedisURI.Builder.redis(_host, _port)
+					.withDatabase(_database)
+					.withTimeout(_timeout)
+					.build()
+		_client = RedisClient.create(uri)
+	}
+
+	fun connect(): StatefulRedisConnection<String, String> {
+		val conn = _client.connect()
+		_conn = conn
+		return conn
+	}
+
+	fun close(conn: StatefulRedisConnection<String, String>? = null) {
+		if(conn != null){
+			conn.close()
+		}else {
+			_conn?.close()
+		}
+		_client.shutdown()
+	}
+
+}
