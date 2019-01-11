a48f198814abda0f38d969edbfaa0a3d450138ed
louis yu <yf_hello@163.com>
2019-01-11 11:45:22 +0800

    wip tinyjdbc

--- tiny/src/main/kotlin/lib/db/JdbcUtil.kt
+++ tiny/src/main/kotlin/lib/db/JdbcUtil.kt
@@ -5,6 +5,34 @@ import java.sql.Blob
 import java.sql.Clob
 
 object JdbcUtil {
+	/*Convert ResultSet to Map<String, Any>*/
+	fun rsToMap(rs: ResultSet): Map<String, Any>{
+		val resultMap = HashMap<String, Any>()
+		val metaData = rs.getMetaData()
+		val columnCount = metaData.getColumnCount()
+		while(rs.next()){
+			for(i in 1..columnCount){
+				resultMap.put(metaData.getColumnName(i), JdbcUtil.getRsValue(rs, i))
+			}
+			break
+		}
+		return resultMap
+	}
+
+	/*Convert ResultSet to List<Map<String, Any>>*/
+	fun rsToList(rs: ResultSet): List<Map<String, Any>>{
+		val resultList: MutableList<HashMap<String, Any>> = mutableListOf()
+		val metaData = rs.getMetaData()
+		val columnCount = metaData.getColumnCount()
+		while(rs.next()){
+			val resultMap = HashMap<String, Any>()
+			for(i in 1..columnCount){
+				resultMap.put(metaData.getColumnName(i), JdbcUtil.getRsValue(rs, i))
+			}
+			resultList.add(resultMap)
+		}
+		return resultList
+	}
 
 	/*copy from org.springframework.jdbc.support.JdbcUtils*/
 	fun getRsValue(rs: ResultSet, index: Int): Any{
--- tiny/src/main/kotlin/lib/db/TinyJdbc.kt
+++ tiny/src/main/kotlin/lib/db/TinyJdbc.kt
@@ -15,11 +15,11 @@ class TinyJdbc {
 	private val _sql = ThreadLocal<String>()
 
 	fun use(dsName: String) {
-		var ds: DataSource? = null
+		var ds: DataSource?
 		try{
 			ds = TinyRegistry["datasource." + dsName] as? DataSource
 		}catch(e: TinyException){
-			//pass
+			throw SQLException("DataSource not found: " + dsName)
 		}
 		if(ds != null){
 			setDataSource(ds)
@@ -30,112 +30,64 @@ class TinyJdbc {
 		_datasource.set(dataSource)
 	}
 
-	fun exec(body: (Connection) -> Any?): Any? {
+	fun <T> exec(body: (Connection) -> T?): T? {
 		val ds = _datasource.get()
 		if(ds == null){
 			throw SQLException("DataSource is null")
 		}
-		var value: Any?
-		var conn: Connection? = null
-		try{
-			conn = ds.getConnection()
-			value = body(conn)
-		}catch(e: Throwable){
-			logger.error("Sql exec error on ${this}", e)
-			throw e
-		}finally{
+		var value: T? = null
+		var conn = ds.getConnection()
+		conn.use{
 			try{
-				conn?.close()
-			}catch(e: Throwable){
-				logger.error("connnection close error", e)
+				value = body(conn)
+			}catch(e: SQLException){
+				logger.error("SQL error on ${this} " + e)
+				throw e
 			}
 		}
 		return value
 	}
 
-	fun queryForList(sql: String, paramMap: Map<String, *>?): List<Map<String, Any>>? {
+	fun <T> query(sql: String, paramMap: Map<String, Any>?, body: (ResultSet) -> T?) : T? {
 		_sql.set(sql)
-		val value = exec({ conn ->
-			var stmt: PreparedStatement? = null
+
+		val exeValue = exec<T>({ conn ->
+			var stmt = conn.prepareStatement(sql)
 			var rs: ResultSet? = null
-			try {
-				stmt = conn.prepareStatement(sql)
-				if(stmt == null){
-					return@exec null
-				}
+			stmt.use{
 				rs = stmt.executeQuery()
-				if(rs == null){
-					return@exec null
-				}
-				val resultList: MutableList<HashMap<String, Any>> = mutableListOf()
-				val metaData = rs.getMetaData()
-				val columnCount = metaData.getColumnCount()
-				while(rs.next()){
-					val resultMap = HashMap<String, Any>()
-					for(i in 1..columnCount){
-						resultMap.put(metaData.getColumnName(i), JdbcUtil.getRsValue(rs, i))
-					}
-					resultList.add(resultMap)
-				}
-				return@exec resultList
-			}finally {
-				try{
-					rs?.close()
-					stmt?.close()
-				}catch(e: Throwable){
-					logger.error("resource close error", e)
-				}
 			}
+			if(rs == null){
+				return@exec null
+			}
+			var value: T? = null
+			rs.use{
+				value = body(rs as ResultSet)
+			}
+			return@exec value
 		})
-		if(value == null){
-			return listOf()
-		}
-		@Suppress("UNCHECKED_CAST")
-		return value as List<Map<String, Any>>
+
+		return exeValue
 	}
 
-	fun queryForMap(sql: String, paramMap: Map<String, *>?): Map<String, Any> {
-		_sql.set(sql)
-		val value = exec({ conn ->
-			var stmt: PreparedStatement? = null
-			var rs: ResultSet? = null
-			try {
-				stmt = conn.prepareStatement(sql)
-				if(stmt == null){
-					return@exec null
-				}
-				rs = stmt.executeQuery()
-				if(rs == null){
-					return@exec null
-				}
-				val resultMap = HashMap<String, Any>()
-				val metaData = rs.getMetaData()
-				val columnCount = metaData.getColumnCount()
-				while(rs.next()){
-					for(i in 1..columnCount){
-						resultMap.put(metaData.getColumnName(i), JdbcUtil.getRsValue(rs, i))
-					}
-					break
-				}
-				return@exec resultMap
-			}finally {
-				try{
-					rs?.close()
-					stmt?.close()
-				}catch(e: Throwable){
-					logger.error("resource close error", e)
-				}
-			}
+	fun queryForList(sql: String, paramMap: Map<String, Any>? = null): List<Map<String, Any>> {
+
+		val value = query<List<Map<String, Any>>>(sql, paramMap, { rs ->
+			JdbcUtil.rsToList(rs)
 		})
-		if(value == null){
-			return mapOf()
-		}
-		@Suppress("UNCHECKED_CAST")
-		return value as Map<String, Any>
+		return value ?: listOf()
+	}
+
+	fun queryForMap(sql: String, paramMap: Map<String, Any>?): Map<String, Any> {
+
+		val value = query<Map<String, Any>>(sql, paramMap, { rs ->
+			JdbcUtil.rsToMap(rs)
+		})
+		return value ?: mapOf()
 	}
 
 	override fun toString(): String {
-		return this::class.java.getSimpleName()+"[${_datasource.get()}, sql=${_sql.get()}]"
+		return this::class.java.getSimpleName()+"[${_sql.get()} , ${_datasource.get()}]"
 	}
 }
 
