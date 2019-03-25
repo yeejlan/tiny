a1ddf11649da2efd82be13f390a796f6f14aab3d
louis yu <yf_hello@163.com>
2019-03-25 10:38:51 +0800

    move LettuceRedisPoolConfig to seperate file

--- tiny/src/main/kotlin/lib/redis/LettuceRedisPool.kt
+++ tiny/src/main/kotlin/lib/redis/LettuceRedisPool.kt
@@ -6,77 +6,11 @@ import io.lettuce.core.RedisClient
 import io.lettuce.core.RedisURI
 
 import org.apache.commons.pool2.impl.GenericObjectPool
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig
-import java.time.Duration
+
 import tiny.*
+import java.time.Duration
 
 private val connectTimeout = Duration.ofSeconds(3)
-private val poolMaxTotal = 10
-private val poolMaxIdle = 10
-private val poolMinIdle = 1
-private val poolMaxWaitMillis = 3000L
-private val poolMinEvictableIdleTimeMillis = 600000L
-private val poolTimeBetweenEvictionRunsMillis = 600000L
-private val defaultHost = "127.0.0.1"
-private val defaultPort = 6379
-private val defaultDatabase = 1
-
-class LettuceRedisPoolConfig() {
-	var host = defaultHost
-	var port = defaultPort
-	var database = defaultDatabase
-	var maxTotal = poolMaxTotal
-	var maxIdle = poolMaxIdle
-	var minIdle = poolMinIdle
-	var poolName: String = ""
-	private val _config = GenericObjectPoolConfig<Any>()
-
-	init{
-		_config.setTestOnBorrow(false)
-		_config.setTestOnReturn(false)
-		_config.setTestWhileIdle(false)
-		_config.setMaxWaitMillis(poolMaxWaitMillis)
-		_config.setMinEvictableIdleTimeMillis(poolMinEvictableIdleTimeMillis)
-		_config.setTimeBetweenEvictionRunsMillis(poolTimeBetweenEvictionRunsMillis)		
-	}
-
-	fun getConfig(): GenericObjectPoolConfig<Any>{
-		return _config
-	}
-
-	fun doConfig(): LettuceRedisPoolConfig{
-		if(maxIdle > maxTotal){
-			maxIdle = maxTotal
-		}
-		if(minIdle > maxIdle){
-			minIdle = maxIdle
-		}
-		_config.setMinIdle(minIdle)
-		_config.setMaxTotal(maxTotal)
-		_config.setMaxIdle(maxIdle)
-		return this
-	}
-
-	fun doConfig(config: TinyConfig, prefix: String): LettuceRedisPoolConfig{
-		poolName = prefix
-		host = config["${prefix}.host"]
-		port = config.getInt("${prefix}.port", defaultPort)
-		database = config.getInt("${prefix}.database", defaultDatabase)
-		maxTotal = config.getInt("${prefix}.lettuce.maxTotal", poolMaxTotal)
-		maxIdle = config.getInt("${prefix}.lettuce.maxIdle", poolMaxIdle)
-		minIdle = config.getInt("${prefix}.lettuce.minIdle", poolMinIdle)
-		return doConfig()
-	}
-
-	fun doFixedPoolConfig(poolSize: Int): LettuceRedisPoolConfig{
-		_config.setMinIdle(1)
-		_config.setMaxTotal(poolSize)
-		_config.setMaxIdle(poolSize)
-		return this
-	}
-}
-
-
 
 class LettuceRedisPool(): LettuceDataSource {
 
