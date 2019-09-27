d759a8dff7ff34e948c025ef35bf86ebb11bb02e
louis yu <yf_hello@163.com>
2019-09-27 09:53:11 +0800

    add toTinyResult

--- tiny/src/main/kotlin/lib/db/SqlResult.kt
+++ tiny/src/main/kotlin/lib/db/SqlResult.kt
@@ -1,3 +1,14 @@
 package tiny.lib.db
 
-data class SqlResult <T> (val ex: Throwable?, val data: T)
+import tiny.TinyResult
+
+data class SqlResult <T> (val ex: Throwable?, val data: T) {
+	fun toTinyResult(): TinyResult<T> {
+		if(this.ex != null) {
+			val msg = this.ex.toString() + ": " + Thread.currentThread().getStackTrace()[2]
+			return TinyResult<T>("Sql query error",  msg)
+		}
+
+		return TinyResult<T>(null, this.data)
+	}
+}
