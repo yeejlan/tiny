647c37fe4be3a75ce664fdc0944493659dff6588
louis yu <yf_hello@163.com>
2019-09-27 11:31:15 +0800

    remove unused code

--- tiny/src/main/kotlin/lib/db/SqlResult.kt
+++ tiny/src/main/kotlin/lib/db/SqlResult.kt
@@ -2,13 +2,4 @@ package tiny.lib.db
 
 import tiny.TinyResult
 
-data class SqlResult <T> (val ex: Throwable?, val data: T) {
-	fun result(): TinyResult<T> {
-		if(this.ex != null) {
-			val msg = this.ex.toString() + ": " + Thread.currentThread().getStackTrace()[2]
-			return TinyResult<T>("Sql query error",  msg)
-		}
-
-		return TinyResult<T>(null, this.data)
-	}
-}
+data class SqlResult <T> (val ex: Throwable?, val data: T)
